0143cbe648644035e52d56ae1560c37ecc4dc177
Daniel McSheehy <dsm140130@utdallas.edu>
2019-01-15 21:16:59 -0600

    Updated installation instructions

--- cnn.py
+++ cnn.py
@@ -1,6 +1,5 @@
 import torch
 
-
 class CNNnet(torch.nn.Module):
     def __init__(self): #TODO: Make Network Not hardcoded
         """
@@ -17,13 +16,8 @@ class CNNnet(torch.nn.Module):
         self.fully_connected = torch.nn.Linear(16, 28) 
 
     def forward(self, traning_data):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Tensors.
-        """
-        conv1_relu = self.conv1(traning_data).clamp(min=0)
-        conv2_relu = self.conv2(conv1_relu).clamp(min=0)  
+        conv1_relu = self.conv1(traning_data).clamp(min=0) #Relu
+        conv2_relu = self.conv2(conv1_relu).clamp(min=0) #Relu
 
         pool_layer = self.pool1(conv2_relu) 
         reshaped_pool = pool_layer.view(-1, 30 * 2 * 15)
@@ -31,29 +25,3 @@ class CNNnet(torch.nn.Module):
         linear_layer = self.linear1(reshaped_pool)
         fc_layer = self.fully_connected(linear_layer)
         return fc_layer
-
-
-# Create random Tensors to hold inputs and outputs
-#x = torch.randn(10, 49)
-# x = torch.randn(28,1,10,49)
-# # Have to be a long one demensional tensor (For CEL)
-# y = torch.autograd.Variable(torch.LongTensor(28).random_(5))
-# # Construct our model by instantiating the class defined above
-# model = CNNnet()
-
-# # Construct our loss function and an Optimizer. The call to model.parameters()
-# # in the SGD constructor will contain the learnable parameters of the two
-# # nn.Linear modules which are members of the model.
-# criterion = torch.nn.CrossEntropyLoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
-# for t in range(500):
-#     # Forward pass: Compute predicted y by passing x to the model
-#     y_pred = model(x)
-#     # Compute and print loss
-#     loss = criterion(y_pred, y)
-#     print(t, loss.item())
-
-#     # Zero gradients, perform a backward pass, and update the weights.
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from cnn import CNNnet
 from train import train
 import torch
 
-x = torch.randn(28,1,10,49)
+example_batch = torch.randn(28,1,10,49)
 # Have to be a long one demensional tensor (For CEL)
-y = torch.autograd.Variable(torch.LongTensor(28).random_(5))
+y_example_truth_vector = torch.autograd.Variable(torch.LongTensor(28).random_(5))
 # Construct our model by instantiating the class defined above
 model = CNNnet()
 
-# net, batch, batch_size, n_epochs, truth_vector, learning_rate
-train(model, x, 1, 500, y, 1e-4)
+#     net,    batch,  batch_size,n_epochs, truth_vector,   learning_rate
+train(model, example_batch, 1, 500, y_example_truth_vector, 1e-4)
