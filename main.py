b02fa23e2344739ba8f6ac1870f303f3828e0afe
Daniel McSheehy <dsm140130@utdallas.edu>
2019-01-15 22:22:44 -0600

    Added test (accuracy not implemented)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from cnn import CNNnet
-from train import train
+from train import train, test
 import torch
 
+#! Batch size: 28
 example_batch = torch.randn(28,1,10,49)
 # Have to be a long one demensional tensor (For CEL)
 y_example_truth_vector = torch.autograd.Variable(torch.LongTensor(28).random_(5))
@@ -9,4 +10,6 @@ y_example_truth_vector = torch.autograd.Variable(torch.LongTensor(28).random_(5)
 model = CNNnet()
 
 #     net,    batch,  batch_size,n_epochs, truth_vector,   learning_rate
-train(model, example_batch, 1, 500, y_example_truth_vector, 1e-4)
+train(model, example_batch, 28, 500, y_example_truth_vector, 1e-4)
+
+test(model, example_batch, y_example_truth_vector)
--- train.py
+++ train.py
@@ -2,15 +2,26 @@ import torch
 
 def train(net, batch, batch_size, n_epochs, truth_vector, learning_rate):
     criterion = torch.nn.CrossEntropyLoss()
+    #Todo: Switch from SGD
     optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
-    for epoch in range(n_epochs):
+    
+    for epoch in range(n_epochs): 
         # Forward pass: Compute predicted y by passing x to the model
         y_pred = net(batch)
         # Compute and print loss
         loss = criterion(y_pred, truth_vector)
-        print(epoch, loss.item())
+        print("Epoch: ", epoch, " Loss: ", loss.item())
 
         # Zero gradients, perform a backward pass, and update the weights.
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+#Todo: Find accuracy equation:
+def test(net, testing_batch, truth_vector):
+    criterion = torch.nn.CrossEntropyLoss()
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = net(testing_batch)
+    # Compute and print loss
+    loss = criterion(y_pred, truth_vector)
+    print("Loss of model: ", loss.item())
