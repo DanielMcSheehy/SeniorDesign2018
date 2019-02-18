222b1f2908108cf79581b6fdbb90d3744adf73fb
Daniel McSheehy <dsm140130@utdallas.edu>
2019-02-18 16:40:46 -0600

    Improved DS_CNN

--- src/ds_cnn.py
+++ src/ds_cnn.py
@@ -2,28 +2,50 @@ import torch
 # Conv -> 4XDWS_CONV -> AVG pool
 # 1x25x64
 class DS_CNNnet(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size=1,stride=1,padding=0,dilation=1,bias=False):
+    def __init__(self):
         """
         Initalize a Convolutional Neural Network with multiple layers. 
         Includes Relu's and pooling. 
         """
-        
+        super(DS_CNNnet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 28, kernel_size=(4,10), stride=1, padding=0)
 
-        self.conv1 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding,dilation,groups=in_channels,bias=bias)
-        self.pointwise = nn.Conv2d(in_channels,out_channels,1,1,0,1,1,bias=bias)
-     
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.pointwise(x)
-        return x
+        self.conv2 = torch.nn.Conv2d(28,28, kernel_size=1, stride = 1,padding = 0, dilation = 1, groups=28)
+        self.pointwise2 = torch.nn.Conv2d(28,28,1,1,0,1,1)
 
+        self.conv3 = torch.nn.Conv2d(28,28, kernel_size=1,stride = 1,padding = 0,  dilation = 1, groups=28)
+        self.pointwise3 = torch.nn.Conv2d(28,28,1,1,0,1,1,)
+
+        self.conv4 = torch.nn.Conv2d(28,28, kernel_size=1,stride = 1,padding = 0,  dilation = 1, groups=28)
+        self.pointwise4 = torch.nn.Conv2d(28,28,1,1,0,1,1)
+
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        self.linear1 = torch.nn.Linear(64*28*3*20, 16) 
+        self.fully_connected = torch.nn.Linear(16, 3) 
+    
     def forward(self, traning_data):
         conv1_relu = self.conv1(traning_data).clamp(min=0) #Relu
 
         conv2_relu = self.conv2(conv1_relu).clamp(min=0) #Relu
-        pool_layer = self.pool1(conv2_relu) 
-        reshaped_pool = pool_layer.view(-1, 30 * 2 * 15)
+        pointwise2 = self.conv2(conv2_relu).clamp(min=0)
+        
+        conv3_relu = self.conv2(pointwise2).clamp(min=0) #Relu
+        pointwise3 = self.conv2(conv3_relu).clamp(min=0)
+
+        conv4_relu = self.conv2(pointwise3).clamp(min=0) #Relu
+        pointwise4 = self.conv2(conv4_relu).clamp(min=0)
+        
+        pool_layer = self.pool1(pointwise4) 
+        reshaped_pool = pool_layer.view(-1, 64*28*3*20)
 
         linear_layer = self.linear1(reshaped_pool)
         fc_layer = self.fully_connected(linear_layer)
         return fc_layer
+
+sample_data = torch.randn(64, 1, 10, 49)
+model = DS_CNNnet()
+
+model.forward(sample_data)
+
+
--- src/main.py
+++ src/main.py
@@ -28,11 +28,12 @@ validation_list, validation_label_list = audio_manager.convert_to_minibatches(va
 
 num_epochs = 100
 for epoch_num in range(num_epochs):
+    mini_batch_list, mini_batch_label = audio_manager.augment_data(mini_batch_list, mini_batch_label, epoch_num)
     for i, batch in enumerate(mini_batch_list):
         train(model, batch, 64, mini_batch_label[i], 1e-4)
 
     if epoch_num % 10 == 0: 
-        print("Epoch #", epoch_num)
+        print("Epoch #", epoch_num + 1)
         test(model, testing_list, testing_label_list)
 
 print("Final validation of model:")
